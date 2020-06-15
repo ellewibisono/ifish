57b622de22bfbdd8702bc7d639fb37f490bf0967
Elle Wibisono <ellewibisono@gmail.com>
2020-06-15 13:54:43 -0400

    Adding time_diff variable and distance to fishing ground

--- R/catch_location.R
+++ R/catch_location.R
@@ -25,17 +25,13 @@ fishinglocations$datetime <- as.Date(fishinglocations$datetime)
 #df containing all the environmental predictors for the top species
 mergedlandings <- merge(x=dffish, y=fishinglocations, by.x=c('first_codrs_picture_date','boat_name'), by.y=c('datetime','boatname'), all=FALSE)
 mergedlandings <- mergedlandings %>% dplyr::select(landing_id,fishing_gear.x, latitude, longitude, depth,
-                                                   gt_estimate.x, first_codrs_picture_date, Aspect, Slope, BPI, BRI, Roughness) %>%
+                                                   gt_estimate.x, first_codrs_picture_date, Aspect, Slope, BPI, BRI, Roughness, distance, time_diff) %>%
   dplyr::distinct(landing_id, .keep_all=TRUE)
 #But not all landings have matching envrionmental variables
-#Total landings=4613
-#Landing with matching spot trace pings=4435
-#unmatched= 1964
-###
 #For the remainging landing_id, use average location and depth based on each boat_name
-#find landing_id that has not been matched and extract that from the df2top data frame
+#find landing_id that has not been matched and extract that from the data frame
 matchedlandingid <- mergedlandings %>% distinct(landing_id)
-#Filter df2top
+#Filter dffish
 unmatchedlandings <- anti_join(dffish, matchedlandingid) %>% distinct(landing_id, .keep_all=TRUE)
 #Get boat names
 unmatchedboat <- unmatchedlandings %>% distinct(boat_name)
@@ -44,14 +40,14 @@ fishinglocationsunmatched <- semi_join(fishinglocations, unmatchedboat, by=c('bo
 #Calculate average lat, long, and depth by boat
 meanstats <- fishinglocationsunmatched %>% group_by(boatname) %>%
   mutate(meanlat=mean(latitude), meanlong=mean(longitude), meandepth=mean(depth), meanasp=mean(Aspect), meanslope=mean(Slope), meanBPI=mean(BPI),
-         meanBRI=mean(BRI), meanrough=mean(Roughness)) %>%
-  dplyr::select(boatname, meanlat, meanlong, meandepth, gt_estimate, fishing_gear, datetime, meanasp, meanslope, meanBPI, meanBRI, meanrough) %>%
+         meanBRI=mean(BRI), meanrough=mean(Roughness), meandist=mean(distance), meantime_diff=mean(time_diff)) %>%
+  dplyr::select(boatname, meanlat, meanlong, meandepth, gt_estimate, fishing_gear, datetime, meanasp, meanslope, meanBPI, meanBRI, meanrough, meandist, meantime_diff) %>%
   distinct(boatname, .keep_all=TRUE)
-#Add mean stats to the unmatchedlanding data frame #####
+#Add mean stats to the unmatchedlanding data frame
 unmatchedlandings$boat_name <- as.factor(unmatchedlandings$boat_name)
 unmatchedstats <-left_join(x=unmatchedlandings, y=meanstats, by=c('boat_name'='boatname'))
 unmatchedstats <- unmatchedstats%>%
-  dplyr::select(landing_id, fishing_gear.x, gt_estimate.x, meanlat, meanlong, meandepth,datetime,meanasp, meanslope, meanBPI, meanBRI, meanrough)
+  dplyr::select(landing_id, fishing_gear.x, gt_estimate.x, meanlat, meanlong, meandepth,datetime,meanasp, meanslope, meanBPI, meanBRI, meanrough, meandist, meantime_diff)
 na_boats <- unmatchedstats %>% filter_all(any_vars(is.na(.))) %>% #filter for any rows WITH NAs
   dplyr::select(landing_id) #this is landing_id that has no fishing info
 checkboatname <- semi_join(dffish, na_boats) %>% distinct(boat_name, registration_port, gt_estimate)
@@ -59,7 +55,7 @@ checkboatname <- semi_join(dffish, na_boats) %>% distinct(boat_name, registratio
 #Remove NAs from environmental
 unmatchedstats <- na.omit(unmatchedstats)
 #Create column order so we can reorder the column names so that the two dataframes will match
-col_order <- c('landing_id','fishing_gear.x','meanlat','meanlong','meandepth','gt_estimate.x','datetime','meanasp','meanslope','meanBPI','meanBRI','meanrough' )
+col_order <- c('landing_id','fishing_gear.x','meanlat','meanlong','meandepth','gt_estimate.x','datetime','meanasp','meanslope','meanBPI','meanBRI','meanrough','meandist','meantime_diff' )
 #ensure that the number of columns in col_order are the same as the number of columns in the data frame
 if(length(col_order) != length(colnames(mergedlandings))){
   stop('matched and unmatched data frame are different lengths, check columns')
